ea3268d213f1900236d0bb730d59f1840aa3ce9d
xiaowei520 <www.sunwei321@qq.com>
2015-12-01 14:21:35 +0800

    修正日志写入

--- ThinkPHP/Library/Think/Log/Driver/File.class.php
+++ ThinkPHP/Library/Think/Log/Driver/File.class.php
@@ -15,7 +15,7 @@ class File {
 
     protected $config  =   array(
         'log_time_format'   =>  ' c ',
-        'log_file_size'     =>  2097152,
+        'log_file_size'     =>  524288000,
         'log_path'          =>  '',
     );
 
@@ -33,14 +33,27 @@ class File {
      */
     public function write($log,$destination='') {
         $now = date($this->config['log_time_format']);
-        if(empty($destination))
+        if(empty($destination)){
             $destination = $this->config['log_path'].date('y_m_d').'.log';
-        if(!is_dir($this->config['log_path'])) {
-            mkdir($this->config['log_path'],0755,true);
+        }
+		// 固定的access.log，每天清空一次
+        $fixed_name_log = $this->config['log_path'].'access.log';
+		// 如果日期日志文件不存在，说明是新的一天开始了，那么就清空$fixed_name_log
+		if (!file_exists($destination)) {
+			$to_be_delete_file = $fixed_name_log . ".old";
+			rename($fixed_name_log, $to_be_delete_file);
+			unlink($to_be_delete_file);
+		}
+        // 自动创建日志目录
+        $log_dir = dirname($destination);
+        if (!is_dir($log_dir)) {
+            mkdir($log_dir, 0755, true);
         }        
         //检测日志文件大小，超过配置大小则备份日志文件重新生成
-        if(is_file($destination) && floor($this->config['log_file_size']) <= filesize($destination) )
-              rename($destination,dirname($destination).'/'.time().'-'.basename($destination));
-        error_log("[{$now}] ".$_SERVER['REMOTE_ADDR'].' '.$_SERVER['REQUEST_URI']."\r\n{$log}\r\n", 3,$destination);
+        if(is_file($destination) && floor($this->config['log_file_size']) <= filesize($destination) ){
+            rename($destination,dirname($destination).'/'.time().'-'.basename($destination));
+        }
+        error_log("[{$now}] ".$_SERVER['REMOTE_ADDR'].' '.$_SERVER['REQUEST_URI']." {$log}\r\n", 3, $destination); // 一行日志，否则太长了。
+        error_log("[{$now}] ".$_SERVER['REMOTE_ADDR'].' '.$_SERVER['REQUEST_URI']." {$log}\r\n", 3, $fixed_name_log); // 一行日志，否则太长了。
     }
 }
