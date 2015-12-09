71cce962ad0fb8f6188e9fcaa7e10da95f77194e
xiaowei520 <www.sunwei321@qq.com>
2015-12-09 14:16:33 +0800

    测试接口

--- Application/Wxapi/Controller/IndexController.class.php
+++ Application/Wxapi/Controller/IndexController.class.php
@@ -8,8 +8,24 @@ class IndexController extends Controller {
 	public function index() {
 		$this->show ( '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是Home模块的Index控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>', 'utf-8' );
 	}
+	
+
+	
+	
+	
 	protected $appid = 'wx025510256321dd39';
 	protected $appsecret = 'd4624c36b6795d1d99dcf0547af5443d';
+	
+	//测试接口
+	public function get_access_token_test(){
+		I('grant_type');
+		I('appid');
+		I('secret');
+		
+		$url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $this->appid . '&secret=' . $this->appsecret;
+		
+		$test = $this->http_get ( $url);
+	}
 	
 	// 获取微信access_token----get请求
 	public function get_access_token() {
