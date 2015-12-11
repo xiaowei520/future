8c552fa1e34fbe7aba6c0c1e71f8c1bb4c59f654
xiaowei520 <www.sunwei321@qq.com>
2015-12-11 12:07:16 +0800

    增加controller

---
+++ Application/Home/Controller/FunctionController.class.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Home\Controller;
+
+use Home\Controller\BaseController;
+use Org\Util\Response;
+
+/***
+ * 功能函数， 首页下面的图片url,进来统一处理。
+ * 
+ */
+class FunctionController extends BaseController {
+	
+	public function open_rili(){
+		$this->display();
+	}
+}
