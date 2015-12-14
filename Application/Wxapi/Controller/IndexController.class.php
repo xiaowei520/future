0498628b1e99d5c73084373e9f85f1eabd558591
xiaowei520 <www.sunwei321@qq.com>
2015-12-14 22:56:56 +0800

    调试微信 api 的click点击事件的触发

--- Application/Wxapi/Controller/IndexController.class.php
+++ Application/Wxapi/Controller/IndexController.class.php
@@ -29,6 +29,9 @@ class IndexController extends Controller {
 				exit ();
 				break;
 			case Wechat::MSGTYPE_EVENT :
+				$event_arr = $this->wchat_obj->getRevEvent();
+				//$event_arr['key']
+				$this->wchat_obj->text ( $event_arr['key'] )->reply ();
 				
 				break;
 			case Wechat::MSGTYPE_IMAGE :
@@ -40,8 +43,12 @@ class IndexController extends Controller {
 		
 	//	$this->show ( '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>[ 您现在访问的是Home模块的Index控制器 ]</div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>', 'utf-8' );
 	}
-	protected $appid = 'wx025510256321dd39';
-	protected $appsecret = 'd4624c36b6795d1d99dcf0547af5443d';
+	public function getMenu(){
+		$menu = $this->wchat_obj->getMenu();
+		var_dump(json_encode($menu,true));
+		
+	}
+
 	
 	// 测试接口
 	public function get_access_token_test() {
