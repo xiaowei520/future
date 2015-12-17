4dd2568594b9d0b5cda32732451adf645bdb0f55
xiaowei520 <www.sunwei321@qq.com>
2015-12-17 17:07:12 +0800

    修改memcached 缓存配置

--- ThinkPHP/Library/Think/Cache/Driver/Memcached.class.php
+++ ThinkPHP/Library/Think/Cache/Driver/Memcached.class.php
@@ -8,7 +8,6 @@
 // +----------------------------------------------------------------------
 // | Author: 何辉 <runphp@qq.com>
 // +----------------------------------------------------------------------
-
 namespace Think\Cache\Driver;
 
 use Memcached as MemcachedResource;
@@ -17,91 +16,141 @@ use Think\Cache;
 /**
  * Memcached缓存驱动
  */
-class Memcached extends Cache
-{
-
-    /**
-     *
-     * @param array $options
-     */
-    public function __construct($options = array())
-    {
-        if ( !extension_loaded('memcached') ) {
-            E(L('_NOT_SUPPERT_').':memcached');
-        }
-
-        $options = array_merge(array(
-            'servers' => C('MEMCACHED_SERVER') ? C('MEMCACHED_SERVER') : null,
-            'lib_options' => C('MEMCACHED_LIB') ? C('MEMCACHED_LIB') : null
-       ), $options);
-
-        $this->options      =   $options;
-        $this->options['expire'] =  isset($options['expire'])?  $options['expire']  :   C('DATA_CACHE_TIME');
-        $this->options['prefix'] =  isset($options['prefix'])?  $options['prefix']  :   C('DATA_CACHE_PREFIX');
-        $this->options['length'] =  isset($options['length'])?  $options['length']  :   0;
-
-        $this->handler      =   new MemcachedResource;
-        $options['servers'] && $this->handler->addServers($options['servers']);
-        $options['lib_options'] && $this->handler->setOptions($options['lib_options']);
-    }
-
-    /**
-     * 读取缓存
-     * @access public
-     * @param string $name 缓存变量名
-     * @return mixed
-     */
-    public function get($name)
-    {
-        N('cache_read',1);
-        $this->handler->get($this->options['prefix'].$name);
-        return $this->handler->get($this->options['prefix'].$name);
-    }
-
-    /**
-     * 写入缓存
-     * @access public
-     * @param string $name 缓存变量名
-     * @param mixed $value  存储数据
-     * @param integer $expire  有效时间（秒）
-     * @return boolean
-     */
-    public function set($name, $value, $expire = null)
-    {
-        N('cache_write',1);
-        if(is_null($expire)) {
-            $expire  =  $this->options['expire'];
-        }
-        $name   =   $this->options['prefix'].$name;
-        if($this->handler->set($name, $value, time() + $expire)) {
-            if($this->options['length']>0) {
-                // 记录缓存队列
-                $this->queue($name);
-            }
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * 删除缓存
-     * @access public
-     * @param string $name 缓存变量名
-     * @return boolean
-     */
-    public function rm($name, $ttl = false) {
-        $name   =   $this->options['prefix'].$name;
-        return $ttl === false ?
-        $this->handler->delete($name) :
-        $this->handler->delete($name, $ttl);
-    }
-
-    /**
-     * 清除缓存
-     * @access public
-     * @return boolean
-     */
-    public function clear() {
-        return $this->handler->flush();
-    }
+class Memcached extends Cache {
+	
+	/**
+	 *
+	 * @param array $options        	
+	 */
+	public function __construct($options = array()) {
+		if (! extension_loaded ( 'memcached' )) {
+			E ( L ( '_NOT_SUPPERT_' ) . ':memcached' );
+		}
+		
+		// $options = array_merge(array(
+		// 'servers' => C('MEMCACHED_SERVER') ? C('MEMCACHED_SERVER') : null,
+		// 'lib_options' => C('MEMCACHED_LIB') ? C('MEMCACHED_LIB') : null
+		// ), $options);
+		
+		// $this->options = $options;
+		// $this->options['expire'] = isset($options['expire'])? $options['expire'] : C('DATA_CACHE_TIME');
+		// $this->options['prefix'] = isset($options['prefix'])? $options['prefix'] : C('DATA_CACHE_PREFIX');
+		// $this->options['length'] = isset($options['length'])? $options['length'] : 0;
+		
+		// $this->handler = new MemcachedResource;
+		// $options['servers'] && $this->handler->addServers($options['servers']);
+		// $options['lib_options'] && $this->handler->setOptions($options['lib_options']);
+		$options = array_merge ( array (
+				
+				'host' => C ( 'MEMCACHED_HOST' ) ? C ( 'MEMCACHED_HOST' ) : '127.0.0.1',
+				
+				'port' => C ( 'MEMCACHED_PORT' ) ? C ( 'MEMCACHED_PORT' ) : '11211',
+				
+				'username' => C ( 'MEMCACHED_USER' ) ? C ( 'MEMCACHED_USER' ) : null,
+				
+				'password' => C ( 'MEMCACHED_PWD' ) ? C ( 'MEMCACHED_PWD' ) : null,
+				
+				'ocs' => C ( 'MEMCACHED_OCS' ) ? C ( 'MEMCACHED_OCS' ) : false,
+				
+				'timeout' => C ( 'DATA_CACHE_TIMEOUT' ) ? C ( 'DATA_CACHE_TIMEOUT' ) : false,
+				
+				'persistent' => false 
+		), $options );
+		
+		$this->options = $options;
+		
+		$this->options ['expire'] = isset ( $options ['expire'] ) ? $options ['expire'] : C ( 'DATA_CACHE_TIME' );
+		
+		$this->options ['prefix'] = isset ( $options ['prefix'] ) ? $options ['prefix'] : C ( 'DATA_CACHE_PREFIX' );
+		
+		$this->options ['length'] = isset ( $options ['length'] ) ? $options ['length'] : 0;
+		
+		$this->handler = new Memcached ();
+		
+		if ($options ['persistent'] && $options ['timeout'] !== false) {
+			
+			$this->handler->setOption ( Memcached::OPT_CONNECT_TIMEOUT, $options ['timeout'] );
+		}
+		
+		// 阿里云OCS
+		
+		if ($options ['ocs']) {
+			
+			$this->handler->setOption ( Memcached::OPT_COMPRESSION, false );
+			
+			$this->handler->setOption ( Memcached::OPT_BINARY_PROTOCOL, true );
+			
+			$this->handler->addServer ( $options ['host'], $options ['port'] );
+			
+			$this->handler->setSaslAuthData ( $options ['username'], $options ['password'] );
+		} else {
+			
+			$this->handler->addServer ( $options ['host'], $options ['port'] );
+		}
+	}
+	
+	/**
+	 * 读取缓存
+	 *
+	 * @access public
+	 * @param string $name
+	 *        	缓存变量名
+	 * @return mixed
+	 */
+	public function get($name) {
+		N ( 'cache_read', 1 );
+		$this->handler->get ( $this->options ['prefix'] . $name );
+		return $this->handler->get ( $this->options ['prefix'] . $name );
+	}
+	
+	/**
+	 * 写入缓存
+	 *
+	 * @access public
+	 * @param string $name
+	 *        	缓存变量名
+	 * @param mixed $value
+	 *        	存储数据
+	 * @param integer $expire
+	 *        	有效时间（秒）
+	 * @return boolean
+	 */
+	public function set($name, $value, $expire = null) {
+		N ( 'cache_write', 1 );
+		if (is_null ( $expire )) {
+			$expire = $this->options ['expire'];
+		}
+		$name = $this->options ['prefix'] . $name;
+		if ($this->handler->set ( $name, $value, time () + $expire )) {
+			if ($this->options ['length'] > 0) {
+				// 记录缓存队列
+				$this->queue ( $name );
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 删除缓存
+	 *
+	 * @access public
+	 * @param string $name
+	 *        	缓存变量名
+	 * @return boolean
+	 */
+	public function rm($name, $ttl = false) {
+		$name = $this->options ['prefix'] . $name;
+		return $ttl === false ? $this->handler->delete ( $name ) : $this->handler->delete ( $name, $ttl );
+	}
+	
+	/**
+	 * 清除缓存
+	 *
+	 * @access public
+	 * @return boolean
+	 */
+	public function clear() {
+		return $this->handler->flush ();
+	}
 }
